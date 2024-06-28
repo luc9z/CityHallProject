b37a34eec1d0d608af8ee2d012c1d7a5e91da42e
Lucas <lucas123melo2017@gmail.com>
2024-06-28 18:23:00 -0300

    Finalizado

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,66 +1,69 @@
-import { useState, createContext, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../services/firebaseConnection";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
+import { useContext } from "react";
 
 export const AuthContext = createContext({});
 
-function AuthProvider({ children }) {
+export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loadingAuth, setLoadingAuth] = useState(true); // Inicie com true para evitar flashes de conteúdo não autenticado
-  const navigate = useNavigate(); // Certifique-se de que useNavigate está sendo usado corretamente
+  const [loadingAuth, setLoadingAuth] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const loadUser = async () => {
+    async function loadUser() {
       const storageUser = localStorage.getItem("cityhallproject-98bd9");
+
       if (storageUser) {
-        setUser(JSON.parse(storageUser));
-        setLoadingAuth(false);
-      } else {
-        // Se não houver usuário armazenado, redirecionar para /login
-        navigate("/login");
-        setLoadingAuth(false);
+        const loggedUser = JSON.parse(storageUser);
+        setUser(loggedUser);
       }
-    };
+    }
 
     loadUser();
-  }, [navigate]);
+  }, []);
 
   const signIn = async (email, password) => {
     setLoadingAuth(true);
+
     try {
-      const { user: authUser } = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const uid = authUser.uid;
+      const value = await signInWithEmailAndPassword(auth, email, password);
+      let uid = value.user.uid;
+
       const docRef = doc(db, "users", uid);
       const docSnap = await getDoc(docRef);
 
-      const data = {
-        uid,
-        firstName: docSnap.data().firstName,
-        lastName: docSnap.data().lastName,
-        email: authUser.email,
-        profilePhoto: docSnap.data().profilePhoto,
-        phone: docSnap.data().phoneNumber,
-        birthDate: docSnap.data().birthDate,
-      };
+      if (docSnap.exists()) {
+        let data = {
+          uid: uid,
+          firstName: docSnap.data().firstName,
+          lastName: docSnap.data().lastName,
+          email: value.user.email,
+          profilePhoto: docSnap.data().profilePhoto,
+          phoneNumber: docSnap.data().phoneNumber,
+          birthDate: docSnap.data().birthDate,
+          saldo: docSnap.data().saldo,
+        };
 
-      setUser(data);
-      storageUser(data);
-      setLoadingAuth(false);
-      toast.success("Bem-vindo de volta!");
-      navigate("/");
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+        toast.success("Bem-vindo de volta!");
+        navigate("/");
+      } else {
+        toast.error("Usuário não encontrado!");
+        setLoadingAuth(false);
+      }
     } catch (error) {
-      console.error(error);
+      console.error("Erro ao entrar:", error);
       toast.error("Campos incorretos");
       setLoadingAuth(false);
     }
@@ -75,44 +78,47 @@ function AuthProvider({ children }) {
     password
   ) => {
     setLoadingAuth(true);
+
     try {
-      const { user: authUser } = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const uid = authUser.uid;
+      const value = await createUserWithEmailAndPassword(auth, email, password);
+      let uid = value.user.uid;
+
       await setDoc(doc(db, "users", uid), {
-        firstName,
-        lastName,
-        phoneNumber,
-        profilePhoto,
-        email: authUser.email,
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        profilePhoto: profilePhoto,
+        email: email,
         birthDate: "",
+        saldo: 0,
       });
 
-      const data = {
-        uid,
-        firstName,
-        lastName,
-        email: authUser.email,
-        profilePhoto,
-        phoneNumber,
+      let data = {
+        uid: uid,
+        firstName: firstName,
+        lastName: lastName,
+        email: value.user.email,
+        profilePhoto: profilePhoto,
+        phoneNumber: phoneNumber,
         birthDate: "",
+        saldo: 0,
       };
-
       setUser(data);
       storageUser(data);
       setLoadingAuth(false);
       toast.success("Usuário registrado!");
       navigate("/");
     } catch (error) {
+      console.error("Erro ao se registrar:", error);
       switch (error.code) {
         case "auth/email-already-in-use":
-          toast.error("E-mail já em uso");
+          toast.error("Email já está em uso");
           break;
         case "auth/invalid-email":
-          toast.error("E-mail inválido");
+          toast.error("Email inválido");
+          break;
+        case "auth/weak-password":
+          toast.error("Senha fraca");
           break;
         default:
           toast.error("Erro ao registrar usuário");
@@ -131,26 +137,43 @@ function AuthProvider({ children }) {
     localStorage.removeItem("cityhallproject-98bd9");
     setUser(null);
     toast.warn("Você não está mais autenticado!");
-    navigate("/login"); // Redireciona para o login ao fazer logout
+    navigate("/login"); // Redirecionar para o login ao sair
+  };
+
+  const updateBalance = async (newBalance) => {
+    try {
+      const userRef = doc(db, "users", user.uid);
+      await updateDoc(userRef, {
+        saldo: newBalance,
+      });
+
+      const updatedUser = { ...user, saldo: newBalance };
+      setUser(updatedUser);
+      storageUser(updatedUser);
+    } catch (error) {
+      console.error("Erro ao atualizar saldo:", error);
+    }
   };
 
   return (
     <AuthContext.Provider
       value={{
         signed: !!user,
-        user,
+        user: user || {},
         signIn,
         logout,
         signUp,
         loadingAuth,
         storageUser,
+        updateBalance,
         setUser,
       }}
     >
-      {!loadingAuth && children}{" "}
-      {/* Renderiza children somente quando a autenticação não estiver carregando */}
+      {children}
     </AuthContext.Provider>
   );
-}
+};
 
-export default AuthProvider;
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthProvider from "../contexts/AuthContext"; // Importe seu provedor de autenticação
+import { AuthProvider } from "../contexts/AuthContext";
 import Dashboard from "../pages/Dashboard";
 import NotFound from "../pages/notFound";
 import Login from "../pages/Login";
 import PontosTroca from "../pages/PontosTroca";
 import Agendamento from "../pages/Agendamento";
 import Inscricao from "../pages/Inscricao";
+import Admin from "../pages/Admin";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <Router>
       <AuthProvider>
+        <ToastContainer />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/pontostroca" element={<PontosTroca />} />
           <Route path="/agendamento" element={<Agendamento />} />
           <Route path="/inscricao" element={<Inscricao />} />
+          <Route path="/admin" element={<Admin />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AuthProvider>
